069df69d271fe6665baed3ab5f617f614e1700b8
Тетяна Жульєва <128372881+TanyaZhulieva@users.noreply.github.com>
2024-05-04 18:01:10 +0200

    redux

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "contacts",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addContact: (state, action) => {},
+    deleteContact: (state, action) => {},
+  },
+});
